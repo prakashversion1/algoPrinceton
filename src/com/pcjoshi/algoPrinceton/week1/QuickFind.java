9c3f02147d9d5e614b9379312b7a331a7a4c77f5
Prakash Joshi <prakashjoshiversion1@gmail.com>
2016-01-26 22:20:50 +0545

    My implementation of the quick find

--- src/com/pcjoshi/algoPrinceton/week1/QuickFind.java
+++ src/com/pcjoshi/algoPrinceton/week1/QuickFind.java
@@ -4,15 +4,27 @@ package com.pcjoshi.algoPrinceton.week1;
  * Created by prakash on 1/26/16 for algoPrinceton Please contact prakashjoshiversion1@gmail.com
  */
 public class QuickFind {
+    private int count;
+    private int [] id ;
     public QuickFind(int N){
-
+        id = new int[N];
+        for (int i=0;i<N;i++){
+            id[i] = i;
+        }
     }
 
     public boolean connected(int a,int b){
-        return true;
+        return id[a] == id[b];
     }
-
+    //(Connect 4 and 3 mens change  )
     public void union(int a,int b){
+        int p = id[a];
+        int q = id[b];
+        for (int i=0;i<id.length;i++){
+            if (id[i] == p){
+                id[i] = q;
+            }
+        }
 
     }
 
